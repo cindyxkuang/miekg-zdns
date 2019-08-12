f81e75cbd55647868244e306b20d566305d6abf9
Cindy Kuang <cindykuang@outlook.com>
2019-08-12 06:14:23 -0700

    initial commit

---
+++ exdns/README.md
@@ -0,0 +1,15 @@
+[![Build Status](https://travis-ci.org/miekg/exdns.svg?branch=master)](https://travis-ci.org/miekg/exdns)
+[![BSD 2-clause license](https://img.shields.io/github/license/miekg/exdns.svg?maxAge=2592000)](https://opensource.org/licenses/BSD-2-Clause)
+
+# Examples made with Go DNS
+
+This repository has a bunch of example programs that
+are made with the https://github.com/miekg/dns Go package.
+
+Currently they include:
+
+* `as112`: an AS112 black hole server
+* `chaos`: show DNS server identity
+* `check-soa`: check the SOA record of zones for all nameservers
+* `q`: dig-like query tool
+* `reflect`: reflection nameserver
---
+++ exdns/client/client.go
@@ -0,0 +1,97 @@
+// Copyright 2011 Miek Gieben. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+// Chaos is a small program that prints the version.bind and hostname.bind
+// for each address of the nameserver given as argument.
+package main
+
+import (
+	"fmt"
+	"log"
+	"net"
+	"sync"
+	"time"
+
+	"github.com/miekg/dns"
+) 
+
+func main() {
+	dns.HandleFunc("miek.nl.", HelloServer)
+	defer dns.HandleRemove("miek.nl.")
+
+	s, addrstr, err := RunLocalUDPServer(":0")
+	fmt.Println(addrstr)
+	if err != nil {
+		log.Fatalf("unable to run test server: %v", err)
+	}
+	defer s.Shutdown()
+
+	m := new(dns.Msg)
+	m.SetQuestion("miek.nl.", dns.TypeSOA)
+
+	c := new(dns.Client)
+	conn, err := c.Dial("8.8.8.8:43")
+	if err != nil {
+		log.Fatalf("failed to dial: %v", err)
+		s.Shutdown()
+	}
+	if conn == nil {
+		log.Fatalf("conn is nil")
+	}
+	fmt.Println("connection 1: ", conn.LocalAddr())
+	conn.Close()
+	
+	conn2, err := c.Dial("8.8.8.8:80")
+	if err != nil {
+		log.Fatalf("failed to dial: %v", err)
+	}
+	if conn2 == nil {
+		log.Fatalf("conn is nil")
+	}
+	fmt.Println("connection 2: ", conn2.LocalAddr())
+}
+
+func HelloServer(w dns.ResponseWriter, req *dns.Msg) {
+	m := new(dns.Msg)
+	m.SetReply(req)
+
+	m.Extra = make([]dns.RR, 1)
+	m.Extra[0] = &dns.TXT{Hdr: dns.RR_Header{Name: m.Question[0].Name, Rrtype: dns.TypeTXT, Class: dns.ClassINET, Ttl: 0}, Txt: []string{"Hello world"}}
+	w.WriteMsg(m)
+}
+
+func RunLocalUDPServer(laddr string) (*dns.Server, string, error) {
+	server, l, _, err := RunLocalUDPServerWithFinChan(laddr)
+
+	return server, l, err
+}
+
+func RunLocalUDPServerWithFinChan(laddr string, opts ...func(*dns.Server)) (*dns.Server, string, chan error, error) {
+	pc, err := net.ListenPacket("udp", laddr)
+	if err != nil {
+		return nil, "", nil, err
+	}
+	server := &dns.Server{PacketConn: pc, ReadTimeout: time.Hour, WriteTimeout: time.Hour}
+
+	waitLock := sync.Mutex{}
+	waitLock.Lock()
+	server.NotifyStartedFunc = waitLock.Unlock
+
+	// fin must be buffered so the goroutine below won't block
+	// forever if fin is never read from. This always happens
+	// in RunLocalUDPServer and can happen in TestShutdownUDP.
+	fin := make(chan error, 1)
+
+	for _, opt := range opts {
+		opt(server)
+	}
+
+	go func() {
+		fin <- server.ActivateAndServe()
+		pc.Close()
+	}()
+
+	waitLock.Lock()
+	return server, pc.LocalAddr().String(), fin, nil
+}
